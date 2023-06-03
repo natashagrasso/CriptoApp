1185565720304f02fd47a37f89461bade6b639c1
natashagrasso <natiimetralle@gmail.com>
2023-06-03 20:06:44 -0300

    criptoTp

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+import { Header } from './components/Header'
+import {Formulario} from './components/Formulario'
+
+function App() {
+
+ /*  state del formulario */  
+}
+
+export default App
---
+++ src/components/Cripto.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const Cripto = ({ resultado }) => {
+    const { name, wind, main } = resultado
+    
+}
